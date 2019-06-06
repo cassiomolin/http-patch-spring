7e573bc55dca71a952f2c1ad5cc060163749791b
Cássio Mazzochi Molin <cassio.molin@gmail.com>
2019-06-06 21:47:07 +0100

    Improvements

--- src/main/java/com/cassiomolin/patch/util/PatchHelper.java
+++ src/main/java/com/cassiomolin/patch/util/PatchHelper.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import javax.json.*;
+import javax.json.JsonMergePatch;
+import javax.json.JsonPatch;
+import javax.json.JsonStructure;
+import javax.json.JsonValue;
 
 @Component
 @RequiredArgsConstructor
@@ -12,12 +15,30 @@ public class PatchHelper {
 
     private final ObjectMapper mapper;
 
+    /**
+     * Performs a JSON Patch operation.
+     *
+     * @param patch      JSON Patch document
+     * @param targetBean object that will be patched
+     * @param clazz      class of the object the will be patched
+     * @param <T>
+     * @return patched object
+     */
     public <T> T patch(JsonPatch patch, T targetBean, Class<T> clazz) {
         JsonStructure target = mapper.convertValue(targetBean, JsonStructure.class);
         JsonValue patched = patch.apply(target);
         return mapper.convertValue(patched, clazz);
     }
 
+    /**
+     * Performs a JSON Merge Patch operation
+     *
+     * @param mergePatch JSON Merge Patch document
+     * @param targetBean object that will be patched
+     * @param clazz      class of the object the will be patched
+     * @param <T>
+     * @return patched object
+     */
     public <T> T mergePatch(JsonMergePatch mergePatch, T targetBean, Class<T> clazz) {
         JsonValue target = mapper.convertValue(targetBean, JsonValue.class);
         JsonValue patched = mergePatch.apply(target);
